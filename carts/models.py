d23ca5d0df158545fa7e1d4f94041d7d455a1693
Erick Naranjo <ariel99.en@gmail.com>
2022-07-10 15:39:06 -0500

    Creacion del modelo de carts

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from store.models import Product
 
 # Create your models here.
+
+class Cart(models.Model):
+    car_id = models.CharField(max_length=250, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.car_id
+
+class CardItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product
