af22dc0a7dff9a12e9759343baa542e15658284e
Erick Naranjo <ariel99.en@gmail.com>
2022-07-16 17:56:33 -0500

    Paginacion en Ecommerce

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from carts.models import CartItem
 from carts.views import _cart_id
 from .models import Product
 from category.models import Category
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 def store(request, category_slug=None):
     categories = None
@@ -14,12 +15,14 @@ def store(request, category_slug=None):
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 5)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
-    
-
     context = {
-        'products': products,
+        #'products': products,
+        'products': paged_products,
         'product_count': product_count,
     }
 
