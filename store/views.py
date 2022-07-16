a5febcaa964de1f8b503adaafa17cdcb01a0de63
Erick Naranjo <ariel99.en@gmail.com>
2022-07-16 18:52:38 -0500

    Busqueda de Productos

--- store/views.py
+++ store/views.py
@@ -1,9 +1,14 @@
+from itertools import product
+from urllib import request
+from venv import create
 from django.shortcuts import get_object_or_404, render
+from tomlkit import key
 from carts.models import CartItem
 from carts.views import _cart_id
 from .models import Product
 from category.models import Category
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 # Create your views here.
 def store(request, category_slug=None):
     categories = None
@@ -11,14 +16,14 @@ def store(request, category_slug=None):
 
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available=True)
+        products = Product.objects.filter(category=categories, is_available=True).order_by('id')
         paginator = Paginator(products, 5)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
 
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
         paginator = Paginator(products, 5)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
@@ -32,6 +37,17 @@ def store(request, category_slug=None):
 
     return render(request, 'store/store.html', context)
 
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products =Product.objects.order_by('-create_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
 
 def product_detail(request, category_slug, product_slug):
     try:
