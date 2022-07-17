b04006d40a380ad8cb98a3279c0065b02591f2f9
Erick Naranjo <ariel99.en@gmail.com>
2022-07-17 11:14:07 -0500

    Crear Modelo variant

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.urls import reverse
 from django.db import models
 from category.models import Category
@@ -22,3 +23,18 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+variation_category = (
+    ('color', 'color'),
+    ('talla', 'talla'),
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return  self.product
