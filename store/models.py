c158ddab83a12de11f4af9d9a8f1134bbbef400b
Erick Naranjo <ariel99.en@gmail.com>
2022-07-17 16:24:18 -0500

    Variations en carrito de compras

--- store/models.py
+++ store/models.py
@@ -45,5 +45,6 @@ class Variation(models.Model):
 
     objects = VariationManager()
 
-    def __unicode__(self):
-        return  self.product
+    def __str__(self):
+        return  f'{self.variation_category} : {self.variation_value}'
+        #return self.variation_category + ':' + self.variation_value
