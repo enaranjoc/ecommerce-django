cbe19b229b1085463427cba5a90dd65ae682ea28
Erick Naranjo <ariel99.en@gmail.com>
2022-07-30 14:19:40 -0500

    Resetear password usando envio de Link al email

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,3 @@
-from atexit import register
 from django.urls import path
 from . import views
 
@@ -8,5 +7,8 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('', views.dashboard, name='dashboard'),
+    path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
+    path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+    path('resetPassword/', views.resetPassword, name='resetPassword'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
